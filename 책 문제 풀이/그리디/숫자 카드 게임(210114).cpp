25f82d9236d5b747839e84b449b1455cd21f6ba0
pieisland <35261724+pieisland@users.noreply.github.com>
2021-01-14 15:59:12 +0900

    Create 숫자 카드 게임(210114).cpp

---
+++ 책 문제 풀이/그리디/숫자 카드 게임(210114).cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int n, m;
+int numbers[100][100];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+	int answer = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> numbers[i][j];
+		}
+	}
+
+	int min = 0;
+	for (int i = 0; i < n; i++) {
+		int tmpMin = 100000;
+		for (int j = 0; j < m; j++) {
+			tmpMin = numbers[i][j] < tmpMin ? numbers[i][j] : tmpMin;
+		}
+		if (tmpMin > min) {
+			min = tmpMin;
+		}
+	}
+
+	cout << min << '\n';
+
+	return 0;
+}
