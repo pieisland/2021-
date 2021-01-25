25c3fad710e8c99ce84f51a048bd2e0575c58c53
pieisland <35261724+pieisland@users.noreply.github.com>
2021-01-25 22:42:50 +0900

    Create 문자열 재정렬(210125).cpp

---
+++ 책 문제 풀이/구현/문자열 재정렬(210125).cpp
@@ -0,0 +1,29 @@
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	cin >> s;
+	int len = s.size();
+	int numSum = 0;
+	vector<char> alpha;
+	for (int i = 0; i < len; i++) {
+		if (s[i] >= '0' && s[i] <= '9') numSum += s[i] - '0';
+		else alpha.push_back(s[i]);
+	}
+	sort(alpha.begin(), alpha.end());
+	
+	for (int i = 0; i < alpha.size(); i++) {
+		cout << alpha[i];
+	}
+	cout << numSum << '\n';
+	
+	return 0;
+}
