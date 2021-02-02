eacd3eb46ddff03af2c684cb8bdc72c734665d98
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-02 15:14:56 +0900

    Create 금광(210202).cpp

---
+++ 책 문제 풀이/다이나믹 프로그래밍/금광(210202).cpp
@@ -0,0 +1,61 @@
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+/*
+dp 계산할 때 행 열의 순서가 중요했다.
+for문에서 열이 먼저 시작하고 행들에 대해 봐야한다.
+*/
+
+int t, n, m;
+int dp[21][21];
+int arr[21][21];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> t;
+	for (int test = 0; test < t; test++) {
+		cin >> n >> m;
+		for (int i = 0; i < 21; i++) {
+			for (int j = 0; j < 21; j++) {
+				dp[i][j] = 0;
+				arr[i][j] = 0;
+			}
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				cin >> arr[i][j];
+			}
+		}
+
+		for (int j = 1; j <= m; j++) {
+			for(int i=1;i<=n;i++) 
+				dp[i][j] = max(dp[i - 1][j - 1], max(dp[i][j - 1], dp[i + 1][j - 1])) + arr[i][j];
+		}
+		
+		/*for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				cout << dp[i][j] << ' ';
+			}
+			cout << endl;
+		}*/
+
+		int answer = 0;
+		for (int i = 1; i <= n; i++) {
+			answer = max(answer, dp[i][m]);
+		}
+		cout << answer << '\n';
+	}
+
+	return 0;
+}
