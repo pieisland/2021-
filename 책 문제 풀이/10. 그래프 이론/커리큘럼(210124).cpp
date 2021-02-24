7c4343dd2d7c00f5577ec177b369a38ce1e9f30f
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-24 18:45:41 +0900

    Update 커리큘럼(210124).cpp

--- 책 문제 풀이/10. 그래프 이론/커리큘럼(210124).cpp
+++ 책 문제 풀이/10. 그래프 이론/커리큘럼(210124).cpp
@@ -83,3 +83,113 @@ int main() {
 
 	return 0;
 }
+
+/*
+210224 다시 풀어보았다.
+
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+와 그래도 혼자 구현했습니다. 안익숙해서 시간이 많이 걸리긴 했지만.
+추카포카.
+알려주는 건 선수 과목인데, 넣는 거는 다음 과목을 넣어야 해서
+거기서 좀 헷갈렸었다.
+
+다음과목을 넣어야 선수 과목이 다음 과목의 차수를 줄여줄 수 있으니
+헷갈려도 더 집중하고 하는 수 밖에.
+
+근데 위상정렬문제는
+차수 배열도 필요하고, 후수 과목 정보도 저장해야되고,
+이 문제의 경우는 시간 정보도 저장하고 해야되어서 되게 뭔가 많이 필요한 문제였다.
+
+테스트케이스)
+5
+10 - 1
+10 1 - 1
+4 1 - 1
+4 3 1 - 1
+3 3 - 1
+
+답)
+10 
+20
+14
+18
+17
+
+
+int n;
+vector<int> subject[501];
+
+int degree[501];
+int times[501];
+int listentime[501];
+
+queue<int> q;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int time;
+		cin >> time;//해당 강의의 시간
+		times[i] = time;//강의 시간
+
+		//선수 강의
+		while (1) {
+			int node;
+			cin >> node;
+			if (node == -1) break;
+
+			//다음 강의 넣어주기
+			subject[node].push_back(i);
+			
+			//선수 과목 개수만큼 차수 올려준다.
+			degree[i]++;
+		}
+
+		//선수과목이 없으면..
+		if (degree[i] == 0) {
+			q.push(i);
+			listentime[i] = times[i];
+		}
+	}
+
+	//degree가 0인 애들이 들어있는데
+	while (!q.empty()) {
+		int cnode = q.front();
+		q.pop();
+
+		//다음 과목들에 대해서.
+		for (int i = 0; i < subject[cnode].size(); i++) {
+			int nnode = subject[cnode][i];
+			//차수 빼주고
+			degree[nnode] -= 1;
+
+			//내 선수 과목들은 거 + 내 과목 듣기 / 이전까지 구해놓은 내 과목 듣는 시간
+			listentime[nnode] = max(listentime[cnode] + times[nnode], listentime[nnode]);
+
+			if (degree[nnode]==0) {
+				q.push(nnode);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		cout << listentime[i] << '\b';
+	}
+
+	return 0;
+}
+*/
