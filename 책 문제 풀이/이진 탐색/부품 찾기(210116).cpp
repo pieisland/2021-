b36dae0ed247a73ceda9860724fac8c523c3f2fa
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-24 15:44:57 +0900

    Update 부품 찾기(210116).cpp

--- 책 문제 풀이/이진 탐색/부품 찾기(210116).cpp
+++ 책 문제 풀이/이진 탐색/부품 찾기(210116).cpp
@@ -61,3 +61,74 @@ int main() {
 
 	return 0;
 }
+
+/*
+210224 다시 풀었다. 딱히 풀이가 다른 건 아닌데. 이전에 푼 게 출력 형식이 틀렸더라 ㅋㅋ...;
+*/
+
+/*
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int n;
+int m;
+int arr[1000000];
+int findObj[100000];
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		//cin >> findObj[i];
+		int obj;
+		cin >> obj;
+
+		int start = 0;
+		int end = n - 1;
+		int mid;
+		bool isFound = false;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] < obj) {
+				//찾는 값보다 작으면
+				start = mid + 1;
+			}
+			else if (arr[mid] > obj) {
+				//찾는 값보다 크다면
+				end = mid - 1;
+			}
+			else {
+				//찾는 값이면
+				isFound = true;
+				break;
+			}
+		}
+		if (isFound) {
+			cout << "yes" << ' ';
+		}
+		else {
+			cout << "no" << ' ';
+		}
+	}
+
+	return 0;
+}
+*/
