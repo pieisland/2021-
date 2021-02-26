f2d3477293ca0303cbb103f4b08d0929f031b828
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-27 00:10:05 +0900

    Update 큰 수의 법칙(210114).cpp

--- 책 문제 풀이/그리디/큰 수의 법칙(210114).cpp
+++ 책 문제 풀이/그리디/큰 수의 법칙(210114).cpp
@@ -47,3 +47,46 @@ int main() {
 
 	return 0;
 }
+
+/*
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+//210227
+//하나하나 하는 식 말고 수학적 계산으로 하는 방식으로 풀었다.
+
+int arr[1000];
+int n, m, k;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	sort(arr, arr + n);
+	int firstBig = arr[n - 1];
+	int secondBig = arr[n - 2];
+
+	int answer = 0;
+	int moc = m / (k + 1);
+	int nam = m % (k + 1);
+	answer += (firstBig * k * moc);
+	answer += (secondBig * moc);
+	answer += firstBig * nam;
+
+	cout << answer;
+
+	return 0;
+}
+*/
