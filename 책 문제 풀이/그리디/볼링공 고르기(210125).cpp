5d8c22b128a08c83943334e20b2338424bd8ac12
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-27 14:37:56 +0900

    Update 볼링공 고르기(210125).cpp

--- 책 문제 풀이/그리디/볼링공 고르기(210125).cpp
+++ 책 문제 풀이/그리디/볼링공 고르기(210125).cpp
@@ -25,6 +25,7 @@ int main() {
 	//,,, 
 	int ans = 0;
 	for (int i = 1; i <= m; i++) {
+		n -= ball[i];
 		ans += ball[i] * n;
 	}
 
@@ -32,3 +33,51 @@ int main() {
 
 	return 0;
 }
+
+/*
+210227 n^2로 풀어버렸는데. 나오긴 하는데. 원하는 건 그게 아니지. n이 1000인 게 부담되는 수인지 모르겠음..
+그리고 사실 이렇게 풀면 m이 전혀 소용이 없으니 이상하긴 하지..
+
+그리고말이다. 내가 이전에 저장해둔 코드가 틀렸었다.
+n에서 해당 수를 빼야되는데 안 뺐더라. 이래놓고 혼자 풀었다고 한 건 뭐니.
+
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int n, m;
+int ball[1000];
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> ball[i];
+	}
+
+	sort(ball, ball + n);
+
+	int cnt = 0;
+	//생각해보니 정렬을 할 필요도 없는 거 아닌가.
+	for (int i = 0; i < n - 1; i++) {
+		int val = ball[i];
+		for (int j = i + 1; j < n; j++) {
+			if (val != ball[j]) cnt++;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
+*/
