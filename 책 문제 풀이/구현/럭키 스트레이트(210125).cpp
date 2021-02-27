203520476e52b295c9fc4d93210831b9dfb476e5
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-27 14:43:58 +0900

    Update 럭키 스트레이트(210125).cpp

--- 책 문제 풀이/구현/럭키 스트레이트(210125).cpp
+++ 책 문제 풀이/구현/럭키 스트레이트(210125).cpp
@@ -23,3 +23,44 @@ int main() {
 
 	return 0;
 }
+
+/*
+210227
+이번에는 for문을 두 개를 썼다.
+
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	cin >> s;
+	int len = s.size();
+	int leftSum = 0;
+	int rightSum = 0;
+	for (int i = 0; i < len / 2; i++) {
+		leftSum += s[i] - '0';
+	}
+	for (int i = len / 2; i < len; i++) {
+		rightSum += s[i] - '0';
+	}
+
+	if (leftSum == rightSum) {
+		cout << "LUCKY";
+	}
+	else cout << "READY";
+
+	return 0;
+}
+*/
