7817eb2e7a85a78f6bac7ff3fd84ca338bbf02a0
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-27 14:24:42 +0900

    Update 문자열 뒤집기(210125).cpp

--- 책 문제 풀이/그리디/문자열 뒤집기(210125).cpp
+++ 책 문제 풀이/그리디/문자열 뒤집기(210125).cpp
@@ -37,3 +37,48 @@ int main() {
 
 	return 0;
 }
+
+/*
+210227
+어차피 똑같이 풀었다.
+
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	cin >> s;
+	int len = s.size();
+
+	int zeroCnt = 0;
+	int oneCnt = 0;
+	int val;
+	if (s[0] == '0') zeroCnt++;
+	else oneCnt++;
+	val = s[0];
+	for (int i = 1; i < len; i++) {
+		if (s[i] != val) {
+			if (s[i] == '0') zeroCnt++;
+			else oneCnt++;
+			val = s[i];
+		}
+	}
+
+	int ans = min(zeroCnt, oneCnt);
+	cout << ans;
+
+	return 0;
+}
+*/
