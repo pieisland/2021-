ef24c78e3703c792a8bcc806aa3fd4a132d132d3
pieisland <35261724+pieisland@users.noreply.github.com>
2021-02-27 13:48:08 +0900

    Update 위에서 아래로(210115).cpp

--- 책 문제 풀이/정렬/위에서 아래로(210115).cpp
+++ 책 문제 풀이/정렬/위에서 아래로(210115).cpp
@@ -32,3 +32,41 @@ int main() {
 
 	return 0;
 }
+
+/*
+210227
+sort를 사용해서 큰 수에서 작은 수로 정렬하는 버전. 이전에 푼거는 출력만 그렇게 한 거고.
+
+#include<cstdio>
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int arr[1000];
+int n;
+
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	sort(arr, arr + n, cmp);
+
+	for (int i = 0; i < n; i++) cout << arr[i] << ' ';
+
+	return 0;
+}
+*/
